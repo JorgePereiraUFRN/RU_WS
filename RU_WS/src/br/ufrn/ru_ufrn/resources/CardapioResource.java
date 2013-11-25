78e58713dcd11e3c06d7ae9bab9f9a426256645c
ronimour <ronimour@gmail.com>
2013-11-25 04:39:31 -0300

    Webservice do cardapio funcionando

--- RU_WS/src/br/ufrn/ru_ufrn/resources/CardapioResource.java
+++ RU_WS/src/br/ufrn/ru_ufrn/resources/CardapioResource.java
@@ -3,6 +3,7 @@ package br.ufrn.ru_ufrn.resources;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -27,8 +28,18 @@ public class CardapioResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Cardapio cardapioDoDia() {
 		
-		return MockCardapio.mock();
+		return new MockCardapio().mockRandom();
 	}
+	
+	@GET
+	@Path("data/{date}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Cardapio cardapioDaData(@PathParam("date")String date) {
+		
+		return new MockCardapio().mock(date);
+	}
+	
+	
 	
 
 }
